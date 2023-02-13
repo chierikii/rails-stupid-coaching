022431a93ff1078c57764d9974e5d808e67b5c5a
Chieri Kii <kiichieri@gmail.com>
2023-02-13 16:26:26 +0900

    stupid

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+    # Generic syntax:
+    # verb "path", to: "controller#action"
+    # root to: "questions#ask"
+    get "/ask", to: "questions#ask", as: :ask
+    get "/answer", to: "questions#answer"
+
 end
